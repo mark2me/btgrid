ab998343dfebcfd0148446fe0f98b9937a866a4a
mark2me <simon@sig.tw>
2016-07-21 09:22:09 +0800

    add chinese language

---
+++ lang/zh.js
@@ -0,0 +1,13 @@
+"use strict"
+
+CKEDITOR.plugins.setLang( 'btgrid', 'zh', {
+	selNumCols: '選擇切割欄位數量',
+	selColsSzie: '選擇變動的作用寬度',
+  genNrRows: '需要重複列數',
+	infoTab: 'Info',
+	createBtGrid: '建立 Bootstrap 表格',
+	editBtGrid: '編輯 Bootstrap 表格',
+	numColsError: '請選擇要切割的欄位數字',
+	numRowsError: '請填入需要重複的列數(數字)',
+	sizeError: '請選擇變動的作用寬度',
+} );
